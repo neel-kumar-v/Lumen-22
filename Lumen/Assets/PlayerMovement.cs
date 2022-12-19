cf37dfd06ed8e2c07217df4e70639ea6f800f52a
Suraj Premchandran <suraj.premchandran@gmail.com>
2022-12-18 22:10:03 -0500

    Level1OnCrack
    Level 1 mostly done, some more furniture needed, and slide physics needed

--- Lumen/Assets/PlayerMovement.cs
+++ Lumen/Assets/PlayerMovement.cs
@@ -18,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+
     // Update is called once per frame
     void Update()
     {
---
+++ Lumen/Assets/TriggerAnimation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor;
+
+    [SerializeField] private string doorOpen = "DoorOpen";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myDoor.Play(doorOpen, 0, 0.0f);
+        }
+    }
+}
