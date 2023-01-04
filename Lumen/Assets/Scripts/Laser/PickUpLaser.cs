1f0fa9a9394503d755fc1479509a766791b10c6c
Suraj Premchandran <suraj.premchandran@gmail.com>
2023-01-03 22:39:46 -0500

    Sort Of Working Pick Up

--- Lumen/Assets/Scripts/Laser/PickUpLaser.cs
+++ Lumen/Assets/Scripts/Laser/PickUpLaser.cs
@@ -10,6 +10,8 @@ public class PickUpLaser : MonoBehaviour
     private LaserBeam beam;
     [SerializeField] private Transform laserStart;
     [SerializeField] public Transform pickedUpObject = null;
+    // [SerializeField] private Transform playerCameraTransform;
+    public Camera cam;
     public LayerMask mask;
 
     private void Update() {
@@ -18,6 +20,7 @@ public class PickUpLaser : MonoBehaviour
         if (pickedUpObject == null) return;
         
         Vector3 dir = (pickedUpObject.position - laserStart.position).normalized;
+        
         beam = new LaserBeam(laserStart.position, dir, material, colors, 1000, laserWidth);
     }
 }
