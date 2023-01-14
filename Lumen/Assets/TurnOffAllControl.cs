b460216770e6c1f942b4d596884039827fe722aa
Suraj Premchandran <suraj.premchandran@gmail.com>
2023-01-13 22:29:30 -0500

    GameDone

--- Lumen/Assets/TurnOffAllControl.cs
+++ Lumen/Assets/TurnOffAllControl.cs
@@ -7,19 +7,11 @@ public class TurnOffAllControl : MonoBehaviour
     public MouseLook mouseLook;
 
     public PlayerMovement playerMovement;
-    // Start is called before the first frame update
-    [SerializeField] private GameObject trigger;
-    [SerializeField] private Animator playerEndAnim;
-    [SerializeField] private string playerMove = "PlayerEndAnim";
-    
+
     private void OnTriggerEnter(Collider other) {
-        // if (locked) return;
         if (other.CompareTag("Player")) return;
         playerMovement.turnOff = true;
         mouseLook.turnOffLook = true;
-        Debug.Log("PlayerMove");
-        playerEndAnim.Play(playerMove, 0, 0.0f);
-        // trigger.SetActive(false);
     }
 }
 
