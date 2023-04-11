419a0a2837045031021f4b7382ee2f53f8bd44d6
Suraj Premchandran <suraj.premchandran@gmail.com>
2023-04-10 23:21:17 -0400

    LevelLock

--- Lumen/Assets/Scripts/TriggerScene.cs
+++ Lumen/Assets/Scripts/TriggerScene.cs
@@ -5,11 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class TriggerScene : MonoBehaviour {
     public SceneFader fader;
+    public int nextSceneLoad;
+
+    void Start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+    
     void OnTriggerEnter(Collider collider) // can be Collider HardDick if you want.. I'm not judging you
     {
-        if(collider.gameObject.CompareTag("Player"))
+        if (collider.gameObject.CompareTag("Player"))
         {
-            fader.FadeTo(SceneManager.GetActiveScene().buildIndex + 1);
+            
+            fader.FadeTo(nextSceneLoad);
+            
+            if(nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+            {
+                PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+            }
         }
     }
 }
