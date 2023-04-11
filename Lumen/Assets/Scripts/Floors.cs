ab3af35f1d07268688d0c1b891b306457215d84f
neel-kumar-v <greenworldwins@gmail.com>
2023-04-10 23:11:48 -0400

    floors

---
+++ Lumen/Assets/Scripts/Floors.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floors : MonoBehaviour {
+    public static GameObject[] floors;
+    
+    // Start is called before the first frame update
+    public void Awake() {
+        floors = FindGameObjectsWithLayer(6);
+    }
+    GameObject[] FindGameObjectsWithLayer (int layer) {
+        GameObject[] goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[]; 
+        List<GameObject> goList = new List<GameObject>(); 
+        for (int i = 0; i < goArray.Length; i++) { 
+            if (goArray[i].layer == layer) { 
+                goList.Add(goArray[i]); 
+            } 
+        } 
+        if (goList.Count == 0) {
+            return null; 
+        } 
+        return goList.ToArray(); 
+    }
+  
+}
